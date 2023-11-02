e98eaf944d071a848b945f881f11956599b25623
tonisco <tonyumeozulu@gmail.com>
2023-11-02 13:34:58 +0100

    rm prod check for sw in webpack

--- web/index.web.js
+++ web/index.web.js
@@ -1,15 +1,15 @@
-import {AppRegistry} from 'react-native';
-import registerServiceWorker from './serviceWorkerRegistration';
-import {name as appName} from '../app.json';
-import App from '../App';
+import { AppRegistry } from "react-native"
+import registerServiceWorker from "./serviceWorkerRegistration"
+import { name as appName } from "../app.json"
+import App from "../App"
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
 AppRegistry.runApplication(appName, {
   initialProps: {},
-  rootTag: document.getElementById('root'),
-});
+  rootTag: document.getElementById("root"),
+})
 
 //  WORKBOX throws warning in watch mode and constantly refreshes
-if (process.env.NODE_ENV === 'production') {
-  registerServiceWorker();
+if (process.env.NODE_ENV === "production") {
+  registerServiceWorker()
 }
